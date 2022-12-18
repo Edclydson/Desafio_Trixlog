87cf8a46b35e596b5c2e8c78c100f4de0fa94386
Edclydson <edclydson.sousa@gmail.com>
2022-12-17 21:35:53 -0300

    :recycle: refactor: Adicionando as anotações do Lombok

--- crud/src/main/java/com/trix/crud/dto/NovoCondutor.java
+++ crud/src/main/java/com/trix/crud/dto/NovoCondutor.java
@@ -1,32 +1,12 @@
 package com.trix.crud.dto;
 
-import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
-import com.trix.crud.modelo.Veiculo;
-
-
-public class NovoCondutor {
+@Getter
+@Setter
+public class NovoCondutor{
     private String nome;
-    private String numCnh;
 
-    private List<Veiculo> lista;
-    
-    public List<Veiculo> getLista() {
-        return lista;
-    }
-    public void setLista(List<Veiculo> lista) {
-        this.lista = lista;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getNumCnh() {
-        return numCnh;
-    }
-    public void setNumCnh(String numCnh) {
-        this.numCnh = numCnh;
-    }
+    private String numCnh;
 }
