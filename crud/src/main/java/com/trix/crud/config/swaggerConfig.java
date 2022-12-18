fb4427f7f32c66fe3300f40d83ce011f5a546841
Edclydson <edclydson.sousa@gmail.com>
2022-12-17 21:29:53 -0300

    :books: docs: Atualizando as configs do swagger e a disposição das informações da API

--- crud/src/main/java/com/trix/crud/config/swaggerConfig.java
+++ crud/src/main/java/com/trix/crud/config/swaggerConfig.java
@@ -5,8 +5,12 @@ import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.service.Contact;
+
 
 @Configuration
 public class swaggerConfig {
@@ -15,10 +19,20 @@ public class swaggerConfig {
     @Bean
     public Docket docket(){
         return new Docket(DocumentationType.SWAGGER_2)
-        .select()
-        .apis(RequestHandlerSelectors.any())
-        .paths(PathSelectors.any())
-        .build();
+                .apiInfo(informacoesDaApi())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.trix.crud.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo informacoesDaApi() {
+        return new ApiInfoBuilder()
+                .title("API Trixlog")
+                .description("Api desenvolvida durante processo seletivo para a empresa Trixlog \n Projeto disponível em: https://github.com/Edclydson/Desafio_Trixlog")
+                .contact(new Contact("Edclydson Sousa","https://linkedin.com/in/edclydson","edclydson.sousa@gmail.com"))
+                .version("1.0.0")
+                .build();
     }
 
 
