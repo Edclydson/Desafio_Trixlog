bc2d5c33110fa368ffbe468bc06fe48f2f7fda34
Edclydson <edclydson.sousa@gmail.com>
2022-12-20 14:24:46 -0300

    :recycle: refactor: Adicionando anotações do Lombok ao DTO do Veiculo

--- crud/src/main/java/com/trix/crud/dto/NovoVeiculo.java
+++ crud/src/main/java/com/trix/crud/dto/NovoVeiculo.java
@@ -1,7 +1,12 @@
 package com.trix.crud.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class NovoVeiculo {
-    private String renavamNovoVeiculo; //vinculado ao veiculo
+    private String renavamNovoVeiculo; 
 
     private String placaNovoVeiculo;
     private String chassiNovoVeiculo;
@@ -12,68 +17,4 @@ public class NovoVeiculo {
     private String dataAquisicaoNovoVeiculo;
     private String nomeCondutorNovoVeiculo;
     private String cnhCondutorNovoVeiculo;
-
-    
-    
-    public String getNomeCondutorNovoVeiculo() {
-        return nomeCondutorNovoVeiculo;
-    }
-    public void setNomeCondutorNovoVeiculo(String nomeCondutorNovoVeiculo) {
-        this.nomeCondutorNovoVeiculo = nomeCondutorNovoVeiculo;
-    }
-    public String getCnhCondutorNovoVeiculo() {
-        return cnhCondutorNovoVeiculo;
-    }
-    public void setCnhCondutorNovoVeiculo(String cnhCondutorNovoVeiculo) {
-        this.cnhCondutorNovoVeiculo = cnhCondutorNovoVeiculo;
-    }
-    public String getRenavamNovoVeiculo() {
-        return renavamNovoVeiculo;
-    }
-    public void setRenavamNovoVeiculo(String renavamNovoVeiculo) {
-        this.renavamNovoVeiculo = renavamNovoVeiculo;
-    }
-    public String getPlacaNovoVeiculo() {
-        return placaNovoVeiculo;
-    }
-    public void setPlacaNovoVeiculo(String placaNovoVeiculo) {
-        this.placaNovoVeiculo = placaNovoVeiculo;
-    }
-    public String getChassiNovoVeiculo() {
-        return chassiNovoVeiculo;
-    }
-    public void setChassiNovoVeiculo(String chassiNovoVeiculo) {
-        this.chassiNovoVeiculo = chassiNovoVeiculo;
-    }
-    public String getAnoModeloNovoVeiculo() {
-        return anoModeloNovoVeiculo;
-    }
-    public void setAnoModeloNovoVeiculo(String anoModeloNovoVeiculo) {
-        this.anoModeloNovoVeiculo = anoModeloNovoVeiculo;
-    }
-    public String getAnoFabricacaoNovoVeiculo() {
-        return anoFabricacaoNovoVeiculo;
-    }
-    public void setAnoFabricacaoNovoVeiculo(String anoFabricacaoNovoVeiculo) {
-        this.anoFabricacaoNovoVeiculo = anoFabricacaoNovoVeiculo;
-    }
-    public String getCorNovoVeiculo() {
-        return corNovoVeiculo;
-    }
-    public void setCorNovoVeiculo(String corNovoVeiculo) {
-        this.corNovoVeiculo = corNovoVeiculo;
-    }
-    public String getUfPlacaNovoVeiculo() {
-        return ufPlacaNovoVeiculo;
-    }
-    public void setUfPlacaNovoVeiculo(String ufPlacaNovoVeiculo) {
-        this.ufPlacaNovoVeiculo = ufPlacaNovoVeiculo;
-    }
-    public String getDataAquisicaoNovoVeiculo() {
-        return dataAquisicaoNovoVeiculo;
-    }
-    public void setDataAquisicaoNovoVeiculo(String dataAquisicaoNovoVeiculo) {
-        this.dataAquisicaoNovoVeiculo = dataAquisicaoNovoVeiculo;
-    }
-    
 }
