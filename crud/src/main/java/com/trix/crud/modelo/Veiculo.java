b6d9815ac1d40cad9c0301a89740e9ffbee86506
Edclydson <edclydson.sousa@gmail.com>
2022-12-20 14:27:01 -0300

    :recycle: refactor: Adicionando @Getter e @Setter

--- crud/src/main/java/com/trix/crud/modelo/Veiculo.java
+++ crud/src/main/java/com/trix/crud/modelo/Veiculo.java
@@ -1,10 +1,15 @@
 package com.trix.crud.modelo;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Getter
+@Setter
 @Entity
 @Table(name="veiculo")
 public class Veiculo {
@@ -26,68 +31,5 @@ public class Veiculo {
 
     private String nomeCondutor;
     private String cnhCondutor;
-    
-    
 
-    public String getNomeCondutor() {
-        return nomeCondutor;
-    }
-    public void setNomeCondutor(String nomeCondutor) {
-        this.nomeCondutor = nomeCondutor;
-    }
-    public String getCnhCondutor() {
-        return cnhCondutor;
-    }
-    public void setCnhCondutor(String cnhCondutor) {
-        this.cnhCondutor = cnhCondutor;
-    }
-    public String getPlaca() {
-        return placa;
-    }
-    public void setPlaca(String placa) {
-        this.placa = placa;
-    }
-    public String getChassi() {
-        return chassi;
-    }
-    public void setChassi(String chassi) {
-        this.chassi = chassi;
-    }
-    public String getRenavam() {
-        return renavam;
-    }
-    public void setRenavam(String renavam) {
-        this.renavam = renavam;
-    }
-    public String getAnoModelo() {
-        return anoModelo;
-    }
-    public void setAnoModelo(String anoModelo) {
-        this.anoModelo = anoModelo;
-    }
-    public String getAnoFabricacao() {
-        return anoFabricacao;
-    }
-    public void setAnoFabricacao(String anoFabricacao) {
-        this.anoFabricacao = anoFabricacao;
-    }
-    public String getCor() {
-        return cor;
-    }
-    public void setCor(String cor) {
-        this.cor = cor;
-    }
-    public String getUfPlaca() {
-        return ufPlaca;
-    }
-    public void setUfPlaca(String ufPlaca) {
-        this.ufPlaca = ufPlaca;
-    }
-    public String getDataAquisicao() {
-        return dataAquisicao;
-    }
-    public void setDataAquisicao(String dataAquisicao) {
-        this.dataAquisicao = dataAquisicao;
-    }
- 
 }
