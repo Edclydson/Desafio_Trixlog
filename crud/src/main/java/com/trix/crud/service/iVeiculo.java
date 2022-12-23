887b0d27cfbb8f103fb3d9b0661161a34dbeca51
Edclydson <edclydson.sousa@gmail.com>
2022-12-22 21:11:38 -0300

    :sparkles: feat: Criação da Interface iVeiculo responsavel pelos metodos de CondutorService

---
+++ crud/src/main/java/com/trix/crud/service/iVeiculo.java
@@ -0,0 +1,35 @@
+package com.trix.crud.service;
+
+import com.trix.crud.dto.NovoVeiculo;
+import com.trix.crud.modelo.Veiculo;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface iVeiculo{
+    ResponseEntity cadastrarNovoVeiculo(NovoVeiculo novoVeiculo);
+
+    List<Veiculo> findAll();
+
+    ResponseEntity buscaVeiculoComRenavam(String renavam);
+
+    ResponseEntity buscaVeiculoComUfDaPlaca(String ufDaPlaca);
+
+    ResponseEntity buscaVeiculoComPlaca(String placa);
+
+    ResponseEntity buscaVeiculosComIntervaloAquisicao(String dataInicial,String dataFinal);
+
+    ResponseEntity alterarDadosVeiculo(Veiculo veiculo);
+
+    ResponseEntity deletarVeiculo(String renavam);
+
+    boolean validaPlaca(String placaVeiculo);
+
+    boolean verificaTamanhoDaPlaca(String placaVeiculo);
+
+    boolean validaRenavam(String renavam);
+
+    boolean validaUf(String uf);
+
+    boolean ufExistente(String uf);
+}
