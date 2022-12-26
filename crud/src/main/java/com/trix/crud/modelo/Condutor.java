302d99ea900ad0164f138f6dfc19e606e5361faf
Edclydson <edclydson.sousa@gmail.com>
2022-12-26 20:12:53 -0300

    :recycle: refactor: Adicionando annotation ApiModel em Condutor

--- crud/src/main/java/com/trix/crud/modelo/Condutor.java
+++ crud/src/main/java/com/trix/crud/modelo/Condutor.java
@@ -1,22 +1,19 @@
 package com.trix.crud.modelo;
 
 
+import io.swagger.annotations.ApiModel;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.List;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
 @Getter
 @Setter
 @Entity
 @Table(name="condutores")
-public class Condutor {
+@ApiModel
+public class Condutor{
     
     @Id
     @Column(length = 11)
