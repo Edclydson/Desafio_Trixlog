5cf83732f583b78820f5bb9f36ca787e7fb24db5
Edclydson <edclydson.sousa@gmail.com>
2022-12-26 20:22:42 -0300

    :recycle: refactor: Separando validações de veiculo para uma interface

---
+++ crud/src/main/java/com/trix/crud/service/interfaces/ValidacoesVeiculosInterface.java
@@ -0,0 +1,18 @@
+package com.trix.crud.service.interfaces;
+
+public interface ValidacoesVeiculosInterface{
+
+    boolean validaPlaca(String placaVeiculo);
+
+    boolean verificaTamanhoDaPlaca(String placaVeiculo);
+
+    boolean validaRenavam(String renavam);
+
+    boolean validaUf(String uf);
+
+    boolean ufExistente(String uf);
+
+    boolean veiculoNaoExiste(String renavam);
+
+    boolean validacoesParaCadastroVeiculo(String renavam, String placa, String ufPlaca);
+}
