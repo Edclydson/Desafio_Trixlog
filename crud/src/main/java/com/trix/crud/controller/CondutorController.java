ebeee71fc99759f83b1a7b3648552182c4133dc2
Edclydson <edclydson.sousa@gmail.com>
2022-12-26 20:01:44 -0300

    :recycle: refactor: Adicionando annotations Api e ApiOperation no controlador de condutores

--- crud/src/main/java/com/trix/crud/controller/CondutorController.java
+++ crud/src/main/java/com/trix/crud/controller/CondutorController.java
@@ -1,32 +1,28 @@
 package com.trix.crud.controller;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.trix.crud.dto.NovoCondutor;
 import com.trix.crud.modelo.Condutor;
 import com.trix.crud.service.CondutorService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/condutores")
+@Api(tags = {"Controlador de Condutores"})
 public class CondutorController {
 
     @Autowired
     CondutorService service;
 
     @PostMapping("/cadastrocondutor")
+    @ApiOperation(value = "Realiza o registro de um novo condutor")
     public ResponseEntity cadastrar(@RequestBody NovoCondutor novoCondutor, UriComponentsBuilder uriBuilder){
         if(service.cadastraNovoCondutor(novoCondutor)){
             var uri = uriBuilder.path("/condutores/cadastrocondutor").build(novoCondutor);
@@ -36,26 +32,31 @@ public class CondutorController {
     }
 
     @GetMapping
+    @ApiOperation(value = "Realiza a listagem de todos os condutores registrados")
     public ResponseEntity<List<Condutor>> listaCondutores(){
         return ResponseEntity.ok(service.consultaTodosCondutores());
     }
 
     @GetMapping("/buscacondutor/{cnh}")
+    @ApiOperation(value = "Realiza a busca de um condutor")
     public ResponseEntity buscaCondutor(@PathVariable String cnh){
         return service.consultaCondutorcnh(cnh);
     }
 
     @PutMapping("/alteracondutor")
+    @ApiOperation(value = "Realiza a alteração no registro de um condutor")
     public ResponseEntity alterar(@RequestBody Condutor condutor){
         return service.alteraCondutor(condutor);
     }
 
     @DeleteMapping("/deletacondutor/{cnh}")
+    @ApiOperation(value = "Realiza a exclusão de um registro de um condutor")
     public ResponseEntity deletar(@PathVariable String cnh){
         return service.deletaCondutor(cnh);
     }
 
     @GetMapping("/condutor/{nome_condutor}")
+    @ApiOperation(value = "Realiza a busca de um condutor pelo nome")
     public ResponseEntity buscanomeCondutor(@PathVariable("nome_condutor") String nomeCondutor){
         return service.buscaNomeCondutor(nomeCondutor);
     }
