70b5e09098532f8e5fc3292a23f1fc4d20bd63e0
Edclydson <edclydson.sousa@gmail.com>
2022-12-26 20:16:47 -0300

    :recycle: refactor: Adicionando annotation ApiModel em Veiculo e adicionando parametros na annotation Column de placa,chassi, ufPlaca e cnhCondutor

--- crud/src/main/java/com/trix/crud/modelo/Veiculo.java
+++ crud/src/main/java/com/trix/crud/modelo/Veiculo.java
@@ -1,35 +1,35 @@
 package com.trix.crud.modelo;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @Entity
 @Table(name="veiculo")
+@ApiModel
 public class Veiculo {
 
     @Id
     @Column(name = "renavam_veiculo", length = 11)
     private String renavam;
 
+    @Column(length = 7, unique = true)
     private String placa;
-    @Column(length = 17)
+    @Column(length = 17, unique = true)
     private String chassi;
     @Column(length = 4)
     private String anoModelo;
     @Column(length = 4)
     private String anoFabricacao;
     private String cor;
+    @Column(length = 2)
     private String ufPlaca;
     private String dataAquisicao;
-
-    private String nomeCondutor;
+    @Column(length = 11)
     private String cnhCondutor;
 
 }
