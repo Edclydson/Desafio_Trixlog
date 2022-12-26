b48adeffd683fe5fa2a23c47b81cc77be3686f81
Edclydson <edclydson.sousa@gmail.com>
2022-12-26 20:04:00 -0300

    :recycle: refactor: Adicionando annotations Api e ApiOperation no controlador de veiculos

--- crud/src/main/java/com/trix/crud/controller/VeiculoController.java
+++ crud/src/main/java/com/trix/crud/controller/VeiculoController.java
@@ -3,56 +3,69 @@ package com.trix.crud.controller;
 import com.trix.crud.dto.NovoVeiculo;
 import com.trix.crud.modelo.Veiculo;
 import com.trix.crud.service.VeiculoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/veiculos")
+@Api(tags = {"Controlador de Veículos"})
 public class VeiculoController {
     
     @Autowired
     VeiculoService service;
 
     @PostMapping("/cadastraveiculo")
-    public void cadastraNovoVeiculo(@RequestBody NovoVeiculo novoVeiculo){
-        service.cadastrarNovoVeiculo(novoVeiculo);
+    @ApiOperation(value = "Realiza o registro de um novo veículo")
+    public ResponseEntity cadastraNovoVeiculo(@RequestBody NovoVeiculo novoVeiculo, UriComponentsBuilder uriBuilder){
+        var uri = uriBuilder.path("/cadastraveiculo").build(novoVeiculo);
+        return service.cadastrarNovoVeiculo(novoVeiculo,uri);
     }
 
     @GetMapping
+    @ApiOperation(value = "Realiza a listagem de todos os veículos cadastrados")
     public ResponseEntity<List<Veiculo>> listaVeiculos(){
         return ResponseEntity.ok(service.findAll());
     }
 
     @GetMapping("/{renavam}")
+    @ApiOperation(value = "Realiza a busca de um único veículo")
     public ResponseEntity buscaveiculo(@PathVariable("renavam") String renavam){
         return service.buscaVeiculoComRenavam(renavam);
     }
 
     @PutMapping("/alteraveiculo")
-    public void alterarveiculo(@RequestBody Veiculo veiculo){
-        service.alterarDadosVeiculo(veiculo);
+    @ApiOperation(value = "Realiza a alteração no registro de um veículo")
+    public ResponseEntity alterarveiculo(@RequestBody NovoVeiculo veiculo){
+        return service.alterarDadosVeiculo(veiculo);
     }
 
     @DeleteMapping("/deletaveiculo/{renavam}")
+    @ApiOperation(value = "Realiza a exclusão de um veículo")
     public ResponseEntity deletarveiculo(@PathVariable("renavam") String renavam){
        return service.deletarVeiculo(renavam);
     }
 
 
     @GetMapping("/placa/{uf}")
+    @ApiOperation(value = "Realiza a busca de veículos pela UF da placa")
     public ResponseEntity buscaufplaca(@PathVariable String uf){
         return service.buscaVeiculoComUfDaPlaca(uf);
     }
     
     @GetMapping("/buscaplaca/{placa}")
+    @ApiOperation(value = "Realiza a busca de um veiculo pela placa")
     public ResponseEntity buscaplaca(@PathVariable String placa) {
         return service.buscaVeiculoComPlaca(placa);
     }
 
     @GetMapping("/intervaloaquisicao/{datainicio}/{datafim}")
+    @ApiOperation(value = "Realiza a busca de veículos pelo intervalo de data da aquisição")
     public List<Veiculo> intervalodataaquisicao(@PathVariable("datainicio") String datainicio,
                                                 @PathVariable("datafim") String datafim){
         return null;//service.buscaVeiculosComIntervaloAquisicao(datainicio, datafim).getBody();
