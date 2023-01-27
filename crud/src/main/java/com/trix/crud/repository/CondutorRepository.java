ed4da91c8b79eebb6f636df8f8aa975ae5137845
Edclydson <edclydson.sousa@gmail.com>
2023-01-27 20:51:06 -0300

    :recycle: refactor: Sobrescrevendo findAll para retornar uma Lista de Condutor

--- crud/src/main/java/com/trix/crud/repository/CondutorRepository.java
+++ crud/src/main/java/com/trix/crud/repository/CondutorRepository.java
@@ -1,11 +1,10 @@
 package com.trix.crud.repository;
 
-import java.util.List;
-
+import com.trix.crud.modelo.Condutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.trix.crud.modelo.Condutor;
+import java.util.List;
 
 @Repository
 public interface CondutorRepository extends CrudRepository<Condutor,String>{
@@ -13,4 +12,7 @@ public interface CondutorRepository extends CrudRepository<Condutor,String>{
     List<Condutor> findByNomeCondutorContaining(String nome_condutor);
     
     List<Condutor> findByNomeCondutor(String nome_condutor);
+
+    @Override
+    List<Condutor> findAll();
 }
